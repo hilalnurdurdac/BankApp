37ce9a2488c8d5c57763113073c979691d27bd3f
seydadoleker <seydadoleker@gmail.com>
2024-05-29 10:20:42 +0300

    [feat] added Service

---
+++ BankApp.Domain/Service/AccountService.cs
@@ -0,0 +1,23 @@
+﻿using BankApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Domain.Service
+{
+    public class AccountService
+    {
+        private List<Account> accounts = new(); 
+
+        public AccountService() 
+        {
+            accounts = new List<Account>();
+        }
+        public void HesapAc (string Name ,double Balance)
+        {
+            
+        }
+    }
+}
