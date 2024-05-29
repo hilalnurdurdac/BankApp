d110dabe501375c4297d01ef5054ea8b4d914578
a-mrv <aydoganmrv@gmail.com>
2024-05-29 10:02:14 +0300

    [Feat] Entities Account Add

--- BankApp.Domain/Entities/Account.cs
+++ BankApp.Domain/Entities/Account.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BankApp.Domain.Entities
 {
-    internal class Account
+    public class Account
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string IdentiNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public string Password { get; set; }
+        public double Balance { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
