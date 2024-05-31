96f40814de21c4dd8256c8a1a7a724f8130c0813
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-05-31 11:36:57 +0300

    [Feat] add phone number update and display option

--- BankApp.Domain/Service/AccountService.cs
+++ BankApp.Domain/Service/AccountService.cs
@@ -7,92 +7,104 @@ using System.Text;
 using System.Threading.Tasks;
 namespace BankApp.Domain.Service;
 public class AccountService
+{
+    private List<Account> accounts = new List<Account>();
+
+    public Account CreateAccount(string name, string surname, string email, string password, string phoneNumber, string identityNumber)
     {
-        private List<Account> accounts = new List<Account>();
-
-        public Account CreateAccount(string name, string surname, string email, string password, string phoneNumber, string identityNumber)
+        Account newAccount = new Account
         {
-            Account newAccount = new Account
+            Id = Guid.NewGuid(),
+            Created = DateTime.Now,
+            Updated = DateTime.Now,
+            Name = name,
+            Surname = surname,
+            Email = email,
+            Password = password,
+            PhoneNumber = phoneNumber,
+            IdentityNumber = identityNumber,
+            Balance = "0",
+            AccountNumber = GenerateAccountNumber()
+        };
+
+        accounts.Add(newAccount);
+        return newAccount;
+    }
+
+    public Account Login(string email, string password)
+    {
+        return accounts.SingleOrDefault(a => a.Email == email && a.Password == password);
+    }
+
+    public Account GetAccountByNumber(string accountNumber)
+    {
+        return accounts.SingleOrDefault(a => a.AccountNumber == accountNumber);
+    }
+
+    public bool DepositMoney(Account account, decimal amount)
+    {
+        if (amount > 0)
+        {
+            decimal currentBalance = decimal.Parse(account.Balance);
+            account.Balance = (currentBalance + amount).ToString();
+            account.Updated = DateTime.Now;
+            return true;
+        }
+        return false;
+    }
+    public bool UpdatePhoneNumber(Account account, string newPhoneNumber)
+    {
+        if (account != null && !string.IsNullOrWhiteSpace(newPhoneNumber))
+        {
+            account.PhoneNumber = newPhoneNumber;
+            account.Updated = DateTime.Now;
+            return true;
+        }
+        return false;
+    }
+
+    public bool WithdrawMoney(Account account, decimal amount)
+    {
+        if (amount > 0)
+        {
+            decimal currentBalance = decimal.Parse(account.Balance);
+            if (currentBalance >= amount)
             {
-                Id = Guid.NewGuid(),
-                Created = DateTime.Now,
-                Updated = DateTime.Now,
-                Name = name,
-                Surname = surname,
-                Email = email,
-                Password = password,
-                PhoneNumber = phoneNumber,
-                IdentityNumber = identityNumber,
-                Balance = "0",
-                AccountNumber = GenerateAccountNumber()
-            };
-
-            accounts.Add(newAccount);
-            return newAccount;
-        }
-
-        public Account Login(string email, string password)
-        {
-            return accounts.SingleOrDefault(a => a.Email == email && a.Password == password);
-        }
-
-        public Account GetAccountByNumber(string accountNumber)
-        {
-            return accounts.SingleOrDefault(a => a.AccountNumber == accountNumber);
-        }
-
-        public bool DepositMoney(Account account, decimal amount)
-        {
-            if (amount > 0)
-            {
-                decimal currentBalance = decimal.Parse(account.Balance);
-                account.Balance = (currentBalance + amount).ToString();
+                account.Balance = (currentBalance - amount).ToString();
                 account.Updated = DateTime.Now;
                 return true;
             }
-            return false;
         }
-
-        public bool WithdrawMoney(Account account, decimal amount)
-        {
-            if (amount > 0)
-            {
-                decimal currentBalance = decimal.Parse(account.Balance);
-                if (currentBalance >= amount)
-                {
-                    account.Balance = (currentBalance - amount).ToString();
-                    account.Updated = DateTime.Now;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public bool TransferMoney(Account senderAccount, Account receiverAccount, decimal amount)
-        {
-            if (amount > 0 && senderAccount != null && receiverAccount != null)
-            {
-                decimal senderBalance = decimal.Parse(senderAccount.Balance);
-                if (senderBalance >= amount)
-                {
-                    senderAccount.Balance = (senderBalance - amount).ToString();
-                    decimal receiverBalance = decimal.Parse(receiverAccount.Balance);
-                    receiverAccount.Balance = (receiverBalance + amount).ToString();
-
-                    senderAccount.Updated = DateTime.Now;
-                    receiverAccount.Updated = DateTime.Now;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        private string GenerateAccountNumber()
-        {
-            Random random = new Random();
-            return random.Next(100000, 999999).ToString();
-        }
+        return false;
     }
-    
+
+    public bool TransferMoney(Account senderAccount, Account receiverAccount, decimal amount)
+    {
+        if (amount > 0 && senderAccount != null && receiverAccount != null)
+        {
+            decimal senderBalance = decimal.Parse(senderAccount.Balance);
+            if (senderBalance >= amount)
+            {
+                senderAccount.Balance = (senderBalance - amount).ToString();
+                decimal receiverBalance = decimal.Parse(receiverAccount.Balance);
+                receiverAccount.Balance = (receiverBalance + amount).ToString();
+
+                senderAccount.Updated = DateTime.Now;
+                receiverAccount.Updated = DateTime.Now;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private string GenerateAccountNumber()
+    {
+        Random random = new Random();
+        return random.Next(100000, 999999).ToString();
+    }
+}
+
+
 
 
--- BankApp.Domain/Service/TransferMoneyService.cs
+++ BankApp.Domain/Service/TransferMoneyService.cs
@@ -3,34 +3,34 @@ using BankApp.Domain.Entities;
 using BankApp.Domain.Service;
 
 public class TransferMoneyService
+{
+    private AccountService accountService;
+
+    public TransferMoneyService(AccountService accountService)
     {
-        private AccountService accountService;
-
-        public TransferMoneyService(AccountService accountService)
-        {
-            this.accountService = accountService;
-        }
-
-        public bool TransferMoney(string senderAccountNumber, string receiverAccountNumber, decimal amount)
-        {
-            Account senderAccount = accountService.GetAccountByNumber(senderAccountNumber);
-            Account receiverAccount = accountService.GetAccountByNumber(receiverAccountNumber);
-
-            if (senderAccount != null && receiverAccount != null && amount > 0)
-            {
-                decimal senderBalance = decimal.Parse(senderAccount.Balance);
-                if (senderBalance >= amount)
-                {
-                    senderAccount.Balance = (senderBalance - amount).ToString();
-                    decimal receiverBalance = decimal.Parse(receiverAccount.Balance);
-                    receiverAccount.Balance = (receiverBalance + amount).ToString();
-
-                    senderAccount.Updated = DateTime.Now;
-                    receiverAccount.Updated = DateTime.Now;
-
-                    return true;
-                }
-            }
-            return false;
-        }
+        this.accountService = accountService;
     }
+
+    public bool TransferMoney(string senderAccountNumber, string receiverAccountNumber, decimal amount)
+    {
+        Account senderAccount = accountService.GetAccountByNumber(senderAccountNumber);
+        Account receiverAccount = accountService.GetAccountByNumber(receiverAccountNumber);
+
+        if (senderAccount != null && receiverAccount != null && amount > 0)
+        {
+            decimal senderBalance = decimal.Parse(senderAccount.Balance);
+            if (senderBalance >= amount)
+            {
+                senderAccount.Balance = (senderBalance - amount).ToString();
+                decimal receiverBalance = decimal.Parse(receiverAccount.Balance);
+                receiverAccount.Balance = (receiverBalance + amount).ToString();
+
+                senderAccount.Updated = DateTime.Now;
+                receiverAccount.Updated = DateTime.Now;
+
+                return true;
+            }
+        }
+        return false;
+    }
+}
